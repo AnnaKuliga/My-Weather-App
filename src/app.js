6aa7f116faae012f02a31905a029d26b49a0ea66
AnnaKuliga <118673659+AnnaKuliga@users.noreply.github.com>
2022-11-25 09:31:07 +0100

    changed the layout of the clock

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ function showFullTime() {
   let currentYear = date.getFullYear();
 
   let currentTime = document.querySelector("#show-time");
-  currentTime.innerHTML = `${currentDay}, <large> ${hours} : ${minutes} : ${seconds}  </large> <br/> ${currentMonth}, ${dateDay}<br/> ${currentYear}`;
+  currentTime.innerHTML = `<strong> ${hours} : ${minutes} : ${seconds} </strong><br/> ${currentDay}<br/> <br/> ${currentMonth}, ${dateDay}<br/> ${currentYear}`;
 }
 showFullTime();
 
